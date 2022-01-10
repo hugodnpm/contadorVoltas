56aeb039b80d4c6653994b84f1b2f9a6216a6da0
hugodnpm <hugosc@gmail.com>
2022-01-10 17:39:37 -0300

    componetizando elementos e criando a função números de voltas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-
+const MostraVoltas = ({voltas}) => {
+  return(
+    <p>{voltas}
+      <br/>
+      Voltas</p>
+  )
+}
+const MostraTempo = ({tempo}) => {
+  return(
+    <p>{tempo} <br/>
+      Tempo médio de voltas</p>
+  )
+}
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 const  App = () => {
+  const [numVolta, setNumVolta] = useState(0)
+  const [tempo, setTempo] = useState(0)
   
+  const increment = () => {
+    setNumVolta(numVolta + 1)
+    console.log('increment')
+  }
+  const decrement = () => {
+    setNumVolta(numVolta - 1)
+    console.log('decrement')
+  }
     return (
       <div>
-        
+      <MostraVoltas voltas={numVolta}/>
+      <Button text='+' onClick= {increment}/>
+      <Button text='-' onClick={decrement}/>
+      <MostraTempo tempo={tempo}/>
+      <Button text='Iniciar'/>
+      <Button text='Reiniciar'/>
       </div>
     )
   }
