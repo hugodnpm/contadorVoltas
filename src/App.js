b5a1a71d02822641ada4bd6097c9b935e7c5b1d1
hugodnpm <hugosc@gmail.com>
2022-01-10 18:29:32 -0300

    finalizado contador de tempo

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ const MostraVoltas = ({voltas}) => {
   )
 }
 const MostraTempo = ({tempo}) => {
+  const minutos = Math.round(tempo / 60)
+  const segundos = tempo % 60
+  const minutosStr = minutos < 10 ? '0' + minutos : minutos 
+  const segundosStr = segundos < 10 ? '0' + segundos : segundos
   return(
-    <p>{tempo} <br/>
+    <p>{`${minutosStr}:${segundosStr}`} <br/>
       Tempo médio de voltas</p>
   )
 }
@@ -17,13 +21,28 @@ const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 const  App = () => {
   const [numVolta, setNumVolta] = useState(0)
+  const [running, setRunning] = useState(false)
   const [tempo, setTempo] = useState(0)
 
   useEffect(() => {
-    setInterval(() => {
-      console.log('chamou!!')
-    },1000)
-  },[])
+    console.log('toggle!!')
+    let timer = null // null é considerado false
+    if(running){
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+          console.log('chamou!!')
+      },10)
+    } 
+    return () => {
+      if(timer){
+        clearInterval(timer)
+      }
+    }   
+  },[running])
+
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
   
   const increment = () => {
     setNumVolta(numVolta + 1)
@@ -33,15 +52,22 @@ const  App = () => {
     setNumVolta(numVolta - 1)
     
   }
+  const reset = () => {
+    setTempo(0)
+    setNumVolta(0)
+  }
   
     return (
       <div>
       <MostraVoltas voltas={numVolta}/>
       <Button text='+' onClick={increment}/>
       <Button text='-' onClick={decrement}/>
-      <MostraTempo tempo={tempo}/>
-      <Button text='Iniciar'/>
-      <Button text='Reiniciar'/>
+      {
+        numVolta > 0 &&
+        <MostraTempo tempo={Math.round(tempo/numVolta)}/>
+      }      
+      <Button text='Iniciar' onClick={toggleRunning}/>
+      <Button text='Reiniciar'onClick={reset}/>
       </div>
     )
   }
